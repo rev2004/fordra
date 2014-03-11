4f872c00ebddfc303b93fcaa411a4ced1a0dba01
priyaarulselvi <priyaarulselvi@gmail.com>
2014-03-11 08:44:30 +0000

    NLDA Result

---
+++ NLDA/nullLDA.R
@@ -0,0 +1,20 @@
+nullLDA <-
+function(path,testpath)
+{
+  readTraining(path)
+  nlda(X,y)
+  fldr<-dir(testpath)
+  for(i in 1:length(fldr))
+  {
+    sub<-fldr[i];
+    test_path<-paste(testpath,sub,sep="/")
+    images<-dir(test_path)
+    for(j in 1:length(images))
+    {
+      filename<-paste(testpath,sub,images[j],sep="/")
+      print(filename)
+      readTesting(filename)
+      recognition(test_fv,Y)
+    }
+}
+}
